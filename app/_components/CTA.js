549002f157e2a5095e37e309440f03244d5825b5
Shek@gith7928 <shekhargrace14>
2025-08-05 14:31:59 +0530

    text changes

--- app/_components/CTA.js
+++ app/_components/CTA.js
@@ -8,8 +8,8 @@ const CTA = () => {
     <div className="max-w-7xl mx-auto px-4 py-8 md:py-16">
       <div className="grid grid-cols-1 md:grid-cols-[3fr_3fr_6fr] gap-8 items-center ">
         <div className="flex flex-col items-start justify-center gap-4 ">
-          <h2 className=" mb-0 h2  uppercase">Get ready to get started</h2>
-          <p>We believe buying should be simple.</p>
+          <h2 className=" mb-0 h2  uppercase">READY TO GET STARTED?</h2>
+          <p>Let Vera do the hard work for you.</p>
 
           <Button
             variant="primary"
@@ -23,18 +23,18 @@ const CTA = () => {
         </div>
         {/* <div className='bg-[#fef6ff] flex items-center justify-center rounded-xl overflow-hidden h-full'> */}
         <Image
-          src="/assets/75e44a29dd5265014dbb4e47e543bc8cc97aaade.jpg"
+          src="/assets/cta2.png"
           alt="Why Choose VERA"
           width={500}
           height={500}
-          className="object-cover h-full w-full"
+          className="object-cover h-full w-full rounded"
         />
         <Image
-          src="/assets/75e44a29dd5265014dbb4e47e543bc8cc97aaade.jpg"
+          src="/assets/cta1.png"
           alt="Why Choose VERA"
           width={500}
           height={500}
-          className="object-cover h-full w-full"
+          className="object-cover h-full w-full rounded"
         />
         {/* </div> */}
       </div>
